1a8f3e434e19851f8949e8d97f97f705bfce6725
Rafael Carvalho <rafaelcarvalhoneto@gmail.com>
2020-07-07 13:25:14 -0300

    detalhe do produto

--- resources/views/detalheProduto.blade.php
+++ resources/views/detalheProduto.blade.php
@@ -1,51 +1,58 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container mt-5">
+    <section class="container mt-3">
         <div class="row">
+            <div class="col-12">
+                <p><small><a href="index">Página inicial</a> > <a href="eletronicos">Eletrônicos</a> > Computador</small></p>
+            </div>
             <div class="col-md-5">
                 <div>
-                    <img class="d-block w-100 .produto" src="/img/computador.jpg" alt="">
+                    <img class="d-block w-100 .produto" src={{asset('img/eletronico-03.png')}} alt="">
                 </div>
             </div>
             <div class="col-md-7 my-3">
-                <h2>Computador</h2>
+                <h2>Playstation 3</h2>
                 <small>Indentificação do Produto</small>
                 <p class="my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati in odit ratione recusandae excepturi voluptates blanditiis ex, iusto veniam voluptas asperiores, assumenda eos. Voluptate omnis deleniti repellendus velit necessitatibus qui!
                 </p>
                 <form action="./carrinho" method="GET" id="formComprar">
-                    <div class="row">
-                        <div class="col-md-6">
-                            
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="inputTamanho">Modelo</label>
-                                        <select class="form-control" name="inputTamanho" id="inputTamanho">
-                                            <option disabled="" selected="">--</option>
-                                            <option value="250">250 GB Ram</option>
-                                            <option value="500">500 GB Ram</option>
-                                            <option value="1000">1T Ram</option>
-                                        </select>
+                    <div class="container">
+                        <div class="row border">
+                            <div class="col-md-4">
+                                
+                                    <div class="form-row mt-2">
+                                        <div class="form-group col-md-12">
+                                            <label for="inputTamanho">Modelo</label>
+                                            <select class="form-control" name="inputTamanho" id="inputTamanho">
+                                                <option disabled="" selected="">--</option>
+                                                <option value="250">250 GB Ram</option>
+                                                <option value="500">500 GB Ram</option>
+                                                <option value="1000">1T Ram</option>
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="quantidade">Quantidade</label>
-                                        <input type="number" class="form-control" id="quantidade" step="1" min="1" value="1" required>
+                                    <div class="form-row">
+                                        <div class="form-group col-md-12">
+                                            <label for="quantidade">Quantidade</label>
+                                            <input type="number" class="form-control" id="quantidade" step="1" min="1" value="1" required>
+                                        </div>
                                     </div>
+    
+                                
+                            </div>
+                                
+                            <div class="col-md-8 d-flex flex-column justify-content-center text-center bg-light ">
+                                <div class="form-group">
+                                    <p class="m-0"  id="precoDetalhe">R$ 1.399,00</p>
+                                    <small>12x de R$ 116,58 s/ juros</small>
                                 </div>
-
-                            
-                        </div>
-                            
-                        <div class="col-md-6 d-flex flex-column justify-content-center">
-                            <div class="form-group">
-                                <p class="card-text" id="preco">R$ 900,00</p>
+                                <div class="form-group m-0">
+                                    <button type="submit" class="mx-auto my-2 btn btn-primary comprar"><i class="fas fa-shopping-cart"></i> comprar</button>
+                                </div>
+    
                             </div>
-                            <div class="form-group">
-                                <button type="submit" class="mx-auto my-2 btn btn-primary btn-lg"><i class="fas fa-shopping-cart"></i> Comprar</button>
-                            </div>
-
                         </div>
+
                     </div>
                 </form>
 
@@ -63,8 +70,8 @@
 
                     <div>
                         
-                        <button class="btn btn-outline-secondary btn-lg btn-block my-2" type="button" data-toggle="collapse" data-target="#titulo1" aria-expanded="false" aria-controls="titulo1">
-                        Ficha Técnica <i class="fas fa-chevron-down ml-3 font-weight-light"></i>
+                        <button class="acordeao my-2" type="button" data-toggle="collapse" data-target="#titulo1" aria-expanded="false" aria-controls="titulo1"><p class="m-0"> Ficha Técnica <i class="fas fa-chevron-down ml-3 font-weight-light"></i></p>
+                       
                         </button>
 
                         <div id="titulo1" class="collapse" aria-labelledby="aba01" data-parent="#accordionTabsProduto">
@@ -98,8 +105,7 @@
                     </div>
 
                     <div>
-                        <button class="btn btn-outline-secondary btn-lg btn-block my-2" type="button" data-toggle="collapse" data-target="#titulo2" aria-expanded="false" aria-controls="titulo2">
-                        Avaliações <i class="fas fa-chevron-down ml-3 font-weight-light"></i>
+                        <button class="acordeao my-2" type="button" data-toggle="collapse" data-target="#titulo2" aria-expanded="false" aria-controls="titulo2"><p class='m-0'>Avaliações <i class="fas fa-chevron-down ml-3 font-weight-light"></i></p>
                         </button>
 
                         <div id="titulo2" class="collapse" aria-labelledby="aba02" data-parent="#accordionTabsProduto">
